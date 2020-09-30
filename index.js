37bd3b574b327e900efb76f5b8a35627316c73ba
James Min <jmin499@gmail.com>
2020-09-30 02:35:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,4 +23,5 @@ function destructivelyRemoveFirstKitten() {
 
 const appendKitten = (name) => [...kittens, name]
 const prependKitten = (name) => [name, ...kittens]
-const removeLastKitten = () => kittens.slice(0, kittens.length - 1)
+const removeLastKitten = () => kittens.slice(0, kittens.length - 1);
+const removeFirstKitten = () => kittens.slice(1)
